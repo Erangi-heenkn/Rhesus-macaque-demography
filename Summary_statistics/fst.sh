61a37e962807c5f679d97608831fbe716b9c0170
Erangi-heenkn <erangiheenkenda@gmail.com>
2026-05-19 09:45:59 -0700

    Add files via upload

---
+++ Summary_statistics/fst.sh
@@ -0,0 +1,14 @@
+##################################### Fst #############################################
+#!/bin/bash
+#SBATCH -N 1        	# number of nodes
+#SBATCH -c 8        	# number of cores
+#SBATCH -t 01-00:00:00   # time in d-hh:mm:ss
+#SBATCH -p general  	# partition
+#SBATCH -q public   	# QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+
+ml vcftools 
+
+vcftools --vcf ../../autosomal_GT.demography.accessible.vcf \
+--weir-fst-pop Chinese.pop.txt --weir-fst-pop Indian.pop.txt --out fst.rhesus.demog
